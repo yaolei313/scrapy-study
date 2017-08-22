f6d18ed2c89caad3305617af16c00714e76f2464
summer <yaolei313@users.noreply.github.com>
2017-08-23 01:10:33 +0800

    tmp

--- scrapystudy/main.py
+++ scrapystudy/main.py
@@ -1,3 +1,4 @@
 from scrapy import cmdline
 #cmdline.execute("scrapy runspider spiders/quotes_spider.py -o quotes.json".split())
-cmdline.execute("scrapy runspider spiders/room_spider.py -o rooms.json".split())
+#cmdline.execute("scrapy runspider spiders/room_spider.py -o rooms.json".split())
+cmdline.execute("scrapy crawl mydomain2 -o mydomain2.json".split())
---
+++ scrapystudy/spiders/mydomain2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+class Mydomain2Spider(CrawlSpider):
+    name = 'mydomain2'
+    allowed_domains = ['example.com']
+    start_urls = ['http://www.example.com']
+
+    rules = (
+        # Extract links matching 'category.php' (but not matching 'subsection.php')
+        # and follow links from them (since no callback means follow=True by default).
+        Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
+
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(LinkExtractor(allow=('item\.php', )), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        self.logger.info('Hi, this is an item page! %s', response.url)
+        item = scrapy.Item()
+        item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+        item['name'] = response.xpath('//td[@id="item_name"]/text()').extract()
+        item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
+        return item
+
